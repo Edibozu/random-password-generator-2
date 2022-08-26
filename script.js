6e13ad9d05605411640654c5d22798294a88ffcd
Eduardo Pineda <edibozu@gmail.com>
2022-08-26 17:59:46 -0400

    wrote variables for the possible character variables in the password

--- script.js
+++ script.js
@@ -7,7 +7,99 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  //ALL YOUR CODE GOES HERE
+  console.log("Hey! You clicked me!");
+
+  // Possible character variables.
+  var lowerCase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  var upperCase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  var special = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "/",
+    "<",
+    ">",
+    "?",
+    "+",
+    "=",
+    ":",
+    ";",
+    "[",
+    "]",
+    "{",
+    "}",
+    "~",
+  ];
+
+
+  return value;
 }
 
 // Add event listener to generate button
